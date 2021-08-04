df8bea6a256f86a915a4aaea2de03d89237be802
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-08-04 16:21:20 -0400

    part 1

--- Lab4_3/Program.cs
+++ Lab4_3/Program.cs
@@ -10,6 +10,16 @@ namespace Lab4_3
         private string pContactEmail;//email of contact @ company
         private string pPhone;//main comp ph#
 
+        //create only 1 constructor that tkaes all 4 fields. no default
+        public Customer( string _Company, string _ContactName, string _ContactEmail, string _Phone)
+        {
+            SetCompany(_Company);
+            SetContactName(_ContactName);
+            SetContactEmail(_ContactEmail);
+            SetPhone(_Phone);
+        }
+
+
         //create a getter and setter for each field:
         public string GetCompany()
         {
@@ -50,6 +60,14 @@ namespace Lab4_3
         {
             pPhone = _Phone;
         }
+
+
+        //add a ToString that prints out all the info about the customer:
+        public override string ToString()
+        {
+            return $"Company: {pCompany} \nContact Name: {pContactName} \nContact Email: {pContactEmail} \nPhone: {pPhone}";
+        }
+
     }
 
 
